8c7f85e4834d735def1a273b7301999fb6e0c581
hoonably <77783081+hoonably@users.noreply.github.com>
2024-09-30 22:06:35 +0900

    C++, Python 기본 틀 생성

---
+++ PS.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define all(v) v.begin(), v.end()
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pii; typedef pair<ll,ll> pll; 
+typedef tuple<int,int,int> tiii;
+const int INF = 0x3f3f3f3f;  // 1061109567
+// const ll INF = 0x3f3f3f3f3f3f3f3f;
+// const int MOD = 1'000'000'007;
+
+/* -----------------------------------------------------
+
+*/
+
+#define MAX 
+
+
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    
+
+    
+    return 0;
+}
