07c50af4ed2cd1506c1ac2179af53d5961a5bf41
Jeonghoon Park <hoonably@gmail.com>
2025-03-30 15:34:18 +0900

    add pow without mod

--- math/GCD-LCM-POW.cpp
+++ math/GCD-LCM-POW.cpp
@@ -30,3 +30,13 @@ long long mod_pow(long long a, long long b, long long mod) {
     }
     return res;
 }
+
+long long pow_fast(long long a, long long b) {
+    long long res = 1;
+    while (b > 0) {
+        if (b & 1) res = res * a;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
